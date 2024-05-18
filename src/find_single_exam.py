62413d581cbfca27878559708a2ee398179d350d
eli-bigman <nutsugah81@gmail.com>
2024-05-18 23:12:53 +0000

    Refactor get_single_exam_details function to improve code readability and maintainability

--- src/find_single_exam.py
+++ src/find_single_exam.py
@@ -4,7 +4,7 @@ import re
 import logging
 import aiohttp
 import asyncio
-import firebase_functions as FB
+from firebase_helper_functions import FirebaseHelperFunctions
 
 
 logging.basicConfig(
@@ -13,7 +13,16 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def get_single_exam_details(user_id: str, id: int, links: list) -> bool | None:
+
+async def binary_search(venues, id):
+            index = bisect_left(venues, id)
+            if index != len(venues) and venues[index] == id:
+                return True
+            else:
+                return False
+            
+
+async def get_single_exam_details(user_id: str, id: int, link: str) -> bool:
     """
     Get the all exams details of a single course.
 
@@ -23,93 +32,97 @@ async def get_single_exam_details(user_id: str, id: int, links: list) -> bool | 
     website and saves the exams details to firebase
     """
     try:
-        no_id_venue = []
-        exact_venues_details = {}
-        venue_not_found = {}
-
         found_exact_venue = False
-
-        # Delete previous data from firebase
-        FB.delete_exams_details(user_id)
-
-        def binary_search(venues, id):
-            index = bisect_left(venues, id)
-            if index != len(venues) and venues[index] == id:
-                return True
-            else:
-                return False
+        firebase = FirebaseHelperFunctions(user_id)
+        
 
         async with aiohttp.ClientSession() as session:
-            for link in links:
-                async with session.get(link) as response:
-                    text = await response.text()
-                    soup = BeautifulSoup(text, 'lxml')
-                    course_name = soup.select(
-                        'div.header span.text-primary')[0].text
-                    table = soup.find('table', class_='table table-striped')
+            async with session.get(link) as response:
+                # response = requests.get(link)
+                text = await response.text()
+                soup = BeautifulSoup(text, 'lxml')
+                course_name = soup.select(
+                    'div.header span.text-primary')[0].text
+                table = soup.find('table', class_='table table-striped')
 
-                    if table:
-                        course_level = table.find(
-                            'td', string='Course Level').find_next_sibling('td').text
+                if table:
+                    course_level = table.find(
+                        'td', string='Course Level').find_next_sibling('td').text
 
-                        exams_status = table.find(
-                            'td', string='Exams Status').find_next_sibling('td').text
+                    exams_status = table.find(
+                        'td', string='Exams Status').find_next_sibling('td').text
 
-                        exam_date = table.find(
-                            'td', string='Exam Date').find_next_sibling('td').text
+                    exam_date = table.find(
+                        'td', string='Exam Date').find_next_sibling('td').text
 
-                        exam_time = table.find(
-                            'td', string='Exams Time').find_next_sibling('td').text
+                    exam_time = table.find(
+                        'td', string='Exams Time').find_next_sibling('td').text
 
-                        campus = table.find(
-                            'td', string='Campus').find_next_sibling('td').text
+                    campus = table.find(
+                        'td', string='Campus').find_next_sibling('td').text
 
-                        venues = table.find(
-                            'td', string='Venue(s) / Index Range').find_next_sibling('td').find_all('li')
+                    venues = table.find(
+                        'td', string='Venue(s) / Index Range').find_next_sibling('td').find_all('li')
 
-                        for venue in venues:
-                            venue_text = venue.text.strip().split("|")
-                            id_range = venue_text[1]
-                            venue_name = venue_text[0]
-                            all_venues = [venue.text.strip(
-                                "[]").replace("[", "") for venue in venues]
+                    for venue in venues:
+                        venue_text = venue.text.strip().split("|")
+                        id_range = venue_text[1]
+                        venue_name = venue_text[0]
+                        all_venues = [venue.text.strip(
+                            "[]").replace("[", "") for venue in venues]
+                        
+                        no_id_venue = []
+                        if id_range == "":
+                            no_id_venue.append(venue_name)
 
-                            if id_range == "":
-                                no_id_venue.append(venue_name)
+                        venue_id_range = re.findall(r"\d+", id_range)
 
-                            venue_id_range = re.findall(r"\d+", id_range)
-
-                            if len(venue_id_range) == 2 and binary_search(list(range(int(venue_id_range[0]), int(venue_id_range[1]) + 1)), id):
-                                exact_venues_details[course_name] = {'Full_Course_Name': course_name, 'Course_Level': course_level, 'Campus': campus,
-                                                                     'Exams_Status': exams_status, 'Exams_Date': exam_date, 'Exams_Time': exam_time,
-                                                                     'Exact_Venue': f"{venue_name} | {id_range}", 'No_ID_Venue': no_id_venue, 'Link': link, }
-                            else:
-                                venue_not_found[course_name] = {'Full_Course_Name': course_name, 'Course_Level': course_level, 'Campus': campus,
-                                                                'Exams_Status': exams_status, 'Exams_Date': exam_date, 'Exams_Time': exam_time,
-                                                                'Exact_Venue': None, 'All_Exams_Venues': all_venues,  'Link': link, }
-
-            if exact_venues_details and response.status == 200:
-                for course, course_details in exact_venues_details.items():
-                    FB.save_exams_details(user_id, course, course_details)
-                found_exact_venue = True
-            else:
-                for course_, course_details_ in venue_not_found.items():
-                    FB.save_exams_details(user_id, course_, course_details_)
-
-            return found_exact_venue
+                        if len(venue_id_range) == 2 and await binary_search(list(range(int(venue_id_range[0]), int(venue_id_range[1]) + 1)), id):
+                            exact_venues_details = {'Full_Course_Name': course_name, 'Course_Level': course_level, 'Campus': campus,
+                                                                    'Exams_Status': exams_status, 'Exams_Date': exam_date, 'Exams_Time': exam_time,
+                                                                    'Exact_Venue': f"{venue_name} | {id_range}", 'No_ID_Venue': no_id_venue, 'Link': link, }
+                            found_exact_venue = True
+                            firebase.save_exact_venue_details(course_name, exact_venues_details)
+                            # logger.info(f'Venue Found ✅: {venue_name} | {id_range}')
+                        else:
+                            venue_not_found_details = {'Full_Course_Name': course_name, 'Course_Level': course_level, 'Campus': campus,
+                                                            'Exams_Status': exams_status, 'Exams_Date': exam_date, 'Exams_Time': exam_time,
+                                                            'Exact_Venue': None, 'All_Exams_Venues': all_venues,  'Link': link, }
+                            firebase.save_exact_venue_not_found_details(course_name,venue_not_found_details)
+                            # logger.info(f'Venue Not Found ❌: {venue_name}')
+                else:
+                    return found_exact_venue
+                return found_exact_venue
 
     except Exception as e:
         logger.error(f'ERROR GETTING SINGLE_EXAMS_DETAIL: {str(e)}')
-        return None
+        return
 
+
+async def main(user_id: str, student_id: int, links: list) -> bool:
+    try:    
+        found_exact_venue = False
+        FirebaseHelperFunctions(user_id).delete_exams_details()
+        
+        for link in links:
+            result = await get_single_exam_details(user_id=user_id, id=student_id, link=link)
+            if result:
+                found_exact_venue = True
+
+        return found_exact_venue
+
+    except Exception as e:
+        logger.error(f'Error proccesing exams details: {str(e)}')
+        return
+    
 
 if __name__ == "__main__":
     exams_links = ['https://sts.ug.edu.gh/timetable/details/UGRC150/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 1/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 2/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 3/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 4/2024-04-03',
-                   'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 5/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 6/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 7/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-CHS-Batch - 1/2024-04-04', 'https://sts.ug.edu.gh/timetable/details/UGRC150-CHS-Batch - 2/2024-04-04']
+    'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 5/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 6/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-Main Campus-Batch 7/2024-04-03', 'https://sts.ug.edu.gh/timetable/details/UGRC150-CHS-Batch - 1/2024-04-04', 'https://sts.ug.edu.gh/timetable/details/UGRC150-CHS-Batch - 2/2024-04-04']
 
     # user_id = "123456789"
-    ID = 10953871
+    ID =  22048836
 
-    UIDs = ["271330483"]
-    for user_id in UIDs:
-        asyncio.run(get_single_exam_details(user_id, ID, exams_links))
+    # UID = "271330483"
+    UID = "123456789"
+    asyncio.run(main(UID, ID, exams_links))
